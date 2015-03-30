5b1f68e202d56eab5c81af743e0ac9b290a4a055
Sylvain Gallois <syl.gallois@gmail.com>
2015-03-30 12:06:33 +0200

    Création de la classe Client

---
+++ src/tpgithub/Client.java
@@ -0,0 +1,16 @@
+package tpgithub;
+
+import java.util.ArrayList;
+
+public class Client {
+	private ArrayList<Compte>lesComptes;
+	private string nom;
+	private string prenom;
+	
+	public int getSoldeGenerale(){
+		return 0;
+	}
+	public boolean getClientCompteStatus(){
+		return true;
+	}
+}
